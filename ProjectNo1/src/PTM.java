5f8227f85b4e32ddc5c221b2525265cb6d1e7619
user <user@JY-20200401AMGO>
2022-02-17 16:01:44 +0900

    2022-02-17-16:00

--- ProjectNo1/src/PTM.java
+++ ProjectNo1/src/PTM.java
@@ -17,8 +17,8 @@ public class PTM {
 		ManagingFrame mngf = new ManagingFrame();
 		AddFrame2 af = new AddFrame2();
 		SetPasswordFrame spf = new SetPasswordFrame();
-//		mf.start();
-//		jf.start();
+		mf.start();
+		jf.start();
 //		of.start();
 //		caf.start();
 //		cjf.start();
