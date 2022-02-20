0d556a5196d9a92b2144d14c3250c902569c3b55
user <user@JY-20200401AMGO>
2022-02-20 22:21:30 +0900

    2022-2-20-20:20

---
+++ ProjectNo1/src/AlreadyJoinedFrame.java
@@ -0,0 +1,44 @@
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class AlreadyJoinedFrame extends AFrame {
+	private Frame f;
+	private Label l;
+	private Button b;
+
+	public void start() {
+		f = new Frame("Error");
+		f.setSize(250, 150);
+		f.setLayout(null);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent E) {
+				f.dispose();
+			}
+		});
+		f.setLocation(screenSize.width / 2 - 300, screenSize.height / 2 - 200);
+
+		l = new Label("해당 직원은 이미 퇴근했습니다.", Label.CENTER);
+		l.setSize(250, 50);
+		l.setLocation(0, 45);
+
+		b = new Button("확인");
+		b.setSize(60, 30);
+		b.setLocation(95, 100);
+		b.addActionListener(this);
+
+		f.add(l);
+		f.add(b);
+		f.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		f.dispose();
+
+	}
+
+}
