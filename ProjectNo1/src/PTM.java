4a7a4e6e63c563ead489d03ced7f404091a013c3
user <user@JY-20200401AMGO>
2022-05-08 16:24:16 +0900

    2022-05-08

--- ProjectNo1/src/PTM.java
+++ ProjectNo1/src/PTM.java
@@ -3,8 +3,8 @@ public class PTM {
 	public static void main(String[] args) {
 		MainFrame mf = new MainFrame();
 		HelpFrame hf = new HelpFrame();
-//		mf.start();
-		hf.start();
+		mf.start();
+//		hf.start();
 
 	}
 }
